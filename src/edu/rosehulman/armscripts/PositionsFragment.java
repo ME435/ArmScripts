c842daea21e3c5258bf453d66c3a86fe37c2a415
Dave Fisher <fisherds@rose-hulman.edu>
2012-12-09 23:53:46 -0500

    Chipping away at the Positions tab
    Starting to create UI elements for the Positions tab.

--- src/edu/rosehulman/armscripts/PositionsFragment.java
+++ src/edu/rosehulman/armscripts/PositionsFragment.java
@@ -5,19 +5,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class PositionsFragment extends Fragment {
-  
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
   }
-  
+
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     return inflater.inflate(R.layout.fragment_positions, container, false);
-}
-  
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    Spinner spinner = (Spinner) getActivity().findViewById(R.id.position_selector);
+    // Create an ArrayAdapter using the string array and a default spinner
+    // layout
+    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+        R.array.planets_array, android.R.layout.simple_spinner_item);
+    // Specify the layout to use when the list of choices appears
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    // Apply the adapter to the spinner
+    spinner.setAdapter(adapter);
+  }
+
   @Override
   public void onPause() {
     super.onPause();
