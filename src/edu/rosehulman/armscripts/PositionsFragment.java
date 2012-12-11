615363fe6141d9d1a9901ba064200fcded7ba56a
Dave Fisher <fisherds@rose-hulman.edu>
2012-12-11 15:18:16 -0500

    Positions portrait UI
    Finished the portrait UI for the Positions tab.  Now I just need to make
    it actually do something.

--- src/edu/rosehulman/armscripts/PositionsFragment.java
+++ src/edu/rosehulman/armscripts/PositionsFragment.java
@@ -1,11 +1,14 @@
 package edu.rosehulman.armscripts;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class PositionsFragment extends Fragment {
@@ -24,14 +27,50 @@ public class PositionsFragment extends Fragment {
   public void onStart() {
     super.onStart();
     Spinner spinner = (Spinner) getActivity().findViewById(R.id.position_selector);
-    // Create an ArrayAdapter using the string array and a default spinner
-    // layout
     ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
-        R.array.planets_array, android.R.layout.simple_spinner_item);
-    // Specify the layout to use when the list of choices appears
+        R.array.planets_array, R.layout.existing_position_item);
     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    // Apply the adapter to the spinner
     spinner.setAdapter(adapter);
+
+    final Button joint1Button = (Button) getActivity().findViewById(R.id.current_position_joint_1_button);
+    final Button joint2Button = (Button) getActivity().findViewById(R.id.current_position_joint_2_button);
+    final Button joint3Button = (Button) getActivity().findViewById(R.id.current_position_joint_3_button);
+    final Button joint4Button = (Button) getActivity().findViewById(R.id.current_position_joint_4_button);
+    final Button joint5Button = (Button) getActivity().findViewById(R.id.current_position_joint_5_button);
+    final Button gripperButton = (Button) getActivity().findViewById(R.id.current_position_gripper_button);
+    joint1Button.setBackgroundResource(R.drawable.yellow_button);
+    joint1Button.setPadding(60, 0, 0, 0);
+    joint2Button.setPadding(60, 0, 0, 0);
+    joint3Button.setPadding(60, 0, 0, 0);
+    joint4Button.setPadding(60, 0, 0, 0);
+    joint5Button.setPadding(60, 0, 0, 0);
+    gripperButton.setPadding(50, 0, 0, 0);
+    
+    OnClickListener jointSelector = new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        joint1Button.setBackgroundResource(R.drawable.black_button);
+        joint2Button.setBackgroundResource(R.drawable.black_button);
+        joint3Button.setBackgroundResource(R.drawable.black_button);
+        joint4Button.setBackgroundResource(R.drawable.black_button);
+        joint5Button.setBackgroundResource(R.drawable.black_button);
+        gripperButton.setBackgroundResource(R.drawable.black_button);
+        v.setBackgroundResource(R.drawable.yellow_button);
+        joint1Button.setPadding(60, 0, 0, 0);
+        joint2Button.setPadding(60, 0, 0, 0);
+        joint3Button.setPadding(60, 0, 0, 0);
+        joint4Button.setPadding(60, 0, 0, 0);
+        joint5Button.setPadding(60, 0, 0, 0);
+        gripperButton.setPadding(50, 0, 0, 0);
+      }
+    };
+    
+    joint1Button.setOnClickListener(jointSelector);
+    joint2Button.setOnClickListener(jointSelector);
+    joint3Button.setOnClickListener(jointSelector);
+    joint4Button.setOnClickListener(jointSelector);
+    joint5Button.setOnClickListener(jointSelector);
+    gripperButton.setOnClickListener(jointSelector);
   }
 
   @Override
